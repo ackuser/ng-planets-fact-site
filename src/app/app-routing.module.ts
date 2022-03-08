29f198a9fdf5ffd477ab945de084c5b8d3aaf8ce
Karim <mail@ackarim.com>
2022-03-08 17:10:14 +0100

    Grid for Desktop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AppComponentDesktopComponent } from './app-component-desktop/app-component-desktop.component';
 import { AppComponentMobileComponent } from './app-component-mobile/app-component-mobile.component';
 import { AppComponent } from './app.component';
 import { AppComponentTabletComponent } from './app-component-tablet/app-component-tablet.component';
@@ -5,6 +6,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'desktop', component: AppComponentDesktopComponent },
   { path: 'tablet', component: AppComponentTabletComponent },
   { path: 'mobile', component: AppComponentMobileComponent },
 ];
